490afbdc291ee00161e4a4708ed32b7f106fc345
Luan <luangiang001@gmail.com>
2026-08-08 22:38:39 -0700

    revised ch4ex2

--- ch4/exer2.py
+++ ch4/exer2.py
@@ -1,7 +1,11 @@
 # **FizzBuzz** — Classic: print numbers 1-100, but "Fizz" for multiples of 3, "Buzz" for multiples of 5, "FizzBuzz" for both.
 
-for i in range(1, 21, 1):
-    if i % 3 == 0:
+max = 100
+
+for i in range(1, max + 1, 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
         print("Fizz")
     elif i % 5 == 0:
         print("Buzz")
